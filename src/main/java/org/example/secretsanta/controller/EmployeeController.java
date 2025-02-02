09ddc21bf43a21e29ae392098120ae9a85edbc39
ahmedagi <agic.ahmed3@gmail.com>
2025-02-02 18:11:00 +0100

    Refactor employee controller

--- src/main/java/org/example/secretsanta/controller/EmployeeController.java
+++ src/main/java/org/example/secretsanta/controller/EmployeeController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/employees")
 public class EmployeeController {
 
     private final EmployeeService employeeService;
@@ -14,18 +15,18 @@ public class EmployeeController {
         this.employeeService = employeeService;
     }
 
-    @GetMapping("/employees")
+    @GetMapping
     public Iterable<Employee> getAllEmployees() {
         return employeeService.getAllEmployees();
     }
 
-    @PostMapping("/employees")
+    @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Employee createEmployee(@RequestBody Employee newEmployee) {
         return employeeService.createEmployee(newEmployee);
     }
 
-    @DeleteMapping("/employees/{id}")
+    @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteEmployee(@PathVariable Long id) {
         employeeService.deleteEmployeeById(id);
