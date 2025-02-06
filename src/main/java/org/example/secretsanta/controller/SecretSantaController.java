78594cd0d3556a880d9a3f699c01c52262bc7abb
ahmedagi <agic.ahmed3@gmail.com>
2025-02-06 12:49:49 +0100

    Return pairs as html page instead of json

--- src/main/java/org/example/secretsanta/controller/SecretSantaController.java
+++ src/main/java/org/example/secretsanta/controller/SecretSantaController.java
@@ -3,12 +3,14 @@ package org.example.secretsanta.controller;
 import org.example.secretsanta.model.SecretSantaPairDTO;
 import org.example.secretsanta.service.SecretSantaService;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
-@RequestMapping("/secret-santa")
+@Controller
+@RequestMapping("/")
 public class SecretSantaController {
 
     private final SecretSantaService secretSantaService;
@@ -18,12 +20,15 @@ public class SecretSantaController {
     }
 
     @GetMapping
-    public List<SecretSantaPairDTO> getPairs() {
-        return secretSantaService.getPairs();
+    public String getPairs(Model model) {
+        List<SecretSantaPairDTO> pairs = secretSantaService.getPairs();
+        model.addAttribute("pairs", pairs);
+        return "secret-santa.html";
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @ResponseBody
     public List<SecretSantaPairDTO> generatePairs() {
         return secretSantaService.generateNewPairs();
     }
